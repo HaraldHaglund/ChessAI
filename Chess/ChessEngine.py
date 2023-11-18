d54faf9bad3ed74f6774d34b07a4b8fa4157db0c
Harald <ha4384ha-s@student.lu.se>
2023-11-18 16:47:50 +0100

    Moving pieces is working. Next step is to fix the code formatting, then implement a control for legal moves!

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -11,14 +11,3 @@ class GameState():
             ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', ],
             ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']
         ]
-        # For the future, this allows us to represent all pieces in 5 bit binary, first two digits represent colour, last three type.
-        # ie 10110 (22 in decimal) would represent a black queen
-        self.none = 0
-        self.king = 1
-        self.pawn = 2
-        self.knight = 3
-        self.bishop = 4
-        self.rook = 5
-        self.queen = 6
-        self.white = 8
-        self.black = 16
--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -29,14 +29,16 @@ def main():
     p.init()
     screen = p.display.set_mode(size=(WIDTH, HEIGHT))
     clock = p.time.Clock()
-    screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
     loadImages()  # Load images (only done once)
     running = True
+    playerClicks = []  # First position represents the selected piece, second represents square to move to.
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                selectAndMovePieces(gs, playerClicks)
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
@@ -48,7 +50,7 @@ Responsible for all the graphics within the game
 
 
 def drawGameState(screen, gs):
-    # Add piece highlight or piece suggestion
+    # Possible to add piece highlight here
     drawBoard(screen)
     drawPieces(screen, gs.board)
 
@@ -59,11 +61,11 @@ Draw the squares on the board
 
 
 def drawBoard(screen):
-    white = [237, 210, 175]
-    black = [173, 129, 90]
+    light = [237, 210, 175]
+    dark = [173, 129, 90]
     for row in range(DIMENSION):
         for col in range(DIMENSION):
-            color = white if (row + col) % 2 == 0 else black
+            color = light if (row + col) % 2 == 0 else dark
             pygame.draw.rect(screen, color, p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
@@ -78,6 +80,75 @@ def drawPieces(screen, board):
             piece = board[row][col]
             if piece != '_':
                 screen.blit(IMAGES[piece], p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+'''
+Selects the move on the game board, and fills playerClicks with these (legal) coordinates.
+The board then gets updated.
+'''
+
+
+def selectAndMovePieces(gs, playerClicks):
+    col_pos = int(p.mouse.get_pos()[0] / SQ_SIZE)
+    row_pos = int(p.mouse.get_pos()[1] / SQ_SIZE)
+    if len(playerClicks) == 0:  # First click
+        firstClick(row_pos, col_pos, playerClicks)
+    elif len(playerClicks) == 1:  # Second click
+        secondClick(row_pos, col_pos, playerClicks)
+        updateGameBoard(gs, playerClicks)
+
+
+'''
+Selects the first click
+'''
+
+
+def firstClick(row_pos, col_pos, playerClicks):
+    if legalFirstMove():  # Ensure square is a piece.
+        pieceCoord = (row_pos, col_pos)
+        playerClicks.append(pieceCoord)
+
+
+'''
+Selects the second click
+'''
+
+
+def secondClick(row_pos, col_pos, playerClicks):
+    if legalSecondMove():
+        pieceCoord = (row_pos, col_pos)
+        playerClicks.append(pieceCoord)
+
+
+'''
+Updates the game board based on the state of playerClicks
+'''
+
+
+def updateGameBoard(gs, playerClicks):
+    # Update game board
+    piece1 = gs.board[playerClicks[0][0]][playerClicks[0][1]]
+    gs.board[playerClicks[0][0]][playerClicks[0][1]] = "_"
+    gs.board[playerClicks[1][0]][playerClicks[1][1]] = piece1
+    playerClicks.clear()  # Empty playerClicks
+
+
+'''
+Ensure that the first click is legal
+'''
+
+
+def legalFirstMove():
+    return True  # gs.board[row_pos][col_pos] != "_"
+
+
+'''
+Ensure that the second click is legal
+'''
+
+
+def legalSecondMove():
+    return True
 
 
 '''
